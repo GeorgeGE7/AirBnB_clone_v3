99f76876f019d5d643873c273fbac6fc6b6e1526
GeorgeEM <george.em.se@gmail.com>
2024-03-30 15:33:27 +0200

    Fix storage count method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -95,8 +95,9 @@ class DBStorage:
         if not cls:
             all_classes_objs = self.all()
             return len(all_classes_objs)
-        if cls in classes.values():
-            all_cls_objs = self.all(cls)
-            return len(all_cls_objs)
+        for cl, value in classes.items():
+            if cls == cl or cls == value:
+                all_cls_objs = self.all(cls)
+                return len(all_cls_objs)
         else:
             return
